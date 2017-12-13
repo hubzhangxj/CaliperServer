9db8ac755f4b321ee8098ec1c0485063310337d6
qiaohua.qiu <487qq@163.com>
2017-12-13 19:36:06 +0800

    数据库model建立

--- account/models.py
+++ account/models.py
@@ -1,6 +1,23 @@
-# -*- coding: utf-8 -*-
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=50,null=False,unique=True)  #用户名
+    name = models.CharField(max_length=255, null=True)
+    role = models.IntegerField(null=False) #用户角色 0-admin 1-普通用户
+    createtime = models.DateTimeField(auto_created=True)  #创建时间
+    company = models.CharField(max_length=255, blank=True, null=True) #公司名称
+    address = models.CharField(max_length=255, blank=True, null=True) #公司地址
+
+    class Meta:
+        managed = False
+        db_table = 'user'
--- task/models.py
+++ task/models.py
@@ -2,5 +2,250 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from account.models import User
 
 # Create your models here.
+class Baseboard(models.Model): #主板
+    name = models.CharField(max_length=50, null=False)  #主板名称
+    manufacturer = models.CharField(max_length=200, blank=True, null=True) #制造商
+    version = models.CharField(max_length=50, blank=True, null=True) #主板版本
+
+    class Meta:
+        managed = False
+        db_table = 'baseboard'
+
+
+class Cache(models.Model): #缓存
+    socketdes = models.CharField(max_length=255, blank=True, null=True) #缓存级别
+    size = models.CharField(max_length=50)  #缓存大小
+    operational = models.CharField(max_length=255, blank=True, null=True) #运作模式
+
+    class Meta:
+        managed = False
+        db_table = 'cacheInfo'
+
+
+class CaseResult(models.Model):  #测试用例得分
+    result = models.FloatField() #得分
+    case = models.ForeignKey(TestCase)  # 测试用例
+    sceResult = models.ForeignKey(ScenarioResult)  # 场景得分
+    caseconfig = models.CharField( blank=True, null=True)  # 执行用例的命令
+    unit = models.CharField(max_length=255, blank=True, null=True) #单位
+
+    class Meta:
+        managed = False
+        db_table = 'caseResult'
+
+
+class Config(models.Model): #配置表
+    hostname = models.CharField(max_length=50, blank=True, null=True)  # 测试名称
+    kernel = models.CharField(max_length=50, blank=True, null=True) #kernel 名称
+    cache = models.ForeignKey(Cache)  # 缓存
+    board = models.ForeignKey(Baseboard)  # 主板
+    sys = models.ForeignKey(System)  # 系统
+    os = models.CharField() #系统名称
+
+    class Meta:
+        managed = False
+        db_table = 'config'
+
+
+class Cpu(models.Model): #Cpu
+    socketdes = models.CharField(max_length=50, blank=True, null=True) #cpu 几级
+    manufacturer = models.CharField(max_length=50, blank=True, null=True) #制造商
+    version = models.CharField(max_length=50, blank=True, null=True) #版本
+    maxspeed = models.CharField(max_length=50, blank=True, null=True)  #最大速度
+    currentspeed = models.CharField(max_length=50, blank=True, null=True)  # 当前速度
+    status = models.CharField(max_length=50, blank=True, null=True) #状态
+    corecount = models.IntegerField()  #核心数目
+    enabledCore = models.IntegerField()  #有效核心数
+    threadcount = models.IntegerField()  # 线程数
+    config = models.ForeignKey(Config)  # 配置
+
+    class Meta:
+        managed = False
+        db_table = 'cpu'
+
+
+class DimResult(models.Model): #维度得分
+    task = models.ForeignKey(Task)  # 测试任务
+    result = models.FloatField() #得分
+    dim = models.ForeignKey(Dimension)  # 维度
+
+    class Meta:
+        managed = False
+        db_table = 'dimResult'
+
+
+# class DimTool(models.Model): #维度
+#     id = models.IntegerField(primary_key=True)
+#     dimid = models.IntegerField(db_column='dimId', blank=True, null=True)  # Field name made lowercase.
+#     toolid = models.IntegerField(db_column='toolId', blank=True, null=True)  # Field name made lowercase.
+#
+#     class Meta:
+#         managed = False
+#         db_table = 'dim_tool'
+
+
+class Dimension(models.Model): #维度表
+    name = models.CharField(max_length=50,null=False) #维度名称
+    desc = models.CharField(max_length=200, blank=True, null=True) #维度描述
+
+    class Meta:
+        managed = False
+        db_table = 'dimension'
+
+
+
+
+
+class Log(models.Model): #测试工具日志
+    toolid = models.ForeignKey(TestTool)  # 测试工具
+    path = models.CharField(max_length=255,null=False) #工具路径
+    content = models.TextField(blank=True, null=True) #解析后的内容
+    task = models.ForeignKey(Task)  # 测试任务
+
+    class Meta:
+        managed = False
+        db_table = 'log'
+
+
+class Memory(models.Model): #内存
+    manufacturer = models.CharField(max_length=255, blank=True, null=True) #制造商
+    size = models.IntegerField() #内存大小
+    type = models.CharField(max_length=50, blank=True, null=True) #类型（DDR3)
+    speed = models.CharField(max_length=50, blank=True, null=True) #速度
+    clockspeed = models.CharField(max_length=50, blank=True, null=True)  #实际读写速度
+    banklocator = models.CharField( max_length=50, blank=True, null=True)  # 内存在单板上的位置
+    config = models.ForeignKey(Config)  # 配置
+
+    class Meta:
+        managed = False
+        db_table = 'memory'
+
+
+class Net(models.Model): #网卡
+    interface = models.CharField(max_length=50, blank=True, null=True) #网卡接口
+    bandwidth = models.CharField(max_length=50, blank=True, null=True)  # 带宽
+    driver = models.CharField(max_length=50, blank=True, null=True) #驱动
+    driverversion = models.CharField(max_length=50, blank=True, null=True)  # 驱动版本
+    protocoltype = models.CharField(max_length=50, blank=True, null=True)  #  网络协议
+    address = models.CharField(max_length=50, blank=True, null=True) #网络地址
+    broadcast = models.CharField(max_length=50, blank=True, null=True) #子网掩码
+    netmask = models.CharField(max_length=50, blank=True, null=True)#网关
+    network = models.CharField(max_length=50, blank=True, null=True)#子网
+    mac = models.CharField(max_length=50, blank=True, null=True)#mac地址
+    config = models.ForeignKey(Config)  # 配置
+
+    class Meta:
+        managed = False
+        db_table = 'net'
+
+
+class Partition(models.Model): #分区
+    name = models.CharField(max_length=255, blank=True, null=True) #分区名称
+    size = models.FloatField() #分区大小
+    storage = models.ForeignKey(Storage)  # 硬盘
+
+    class Meta:
+        managed = False
+        db_table = 'partition'
+
+
+class Scenario(models.Model): #场景
+    name = models.CharField(max_length=50,  null=False) #场景名称
+    dim = models.ForeignKey(Dimension)  # 维度
+    desc = models.CharField(max_length=200, blank=True, null=True) #场景描述
+    parentid = models.IntegerField(default=-1)  # 父场景名称
+
+    class Meta:
+        managed = False
+        db_table = 'scenario'
+
+
+class ScenarioResult(models.Model): #场景得分
+    result = models.FloatField() #得分
+    dimresult = models.ForeignKey(DimResult)  # 维度得分
+    scenarioid = models.ForeignKey(Scenario)  # 场景
+
+    class Meta:
+        managed = False
+        db_table = 'scenarioResult'
+
+
+# class Sharetask(models.Model): #任务共享表
+#     taskid = models.ForeignKey('Task', models.DO_NOTHING, db_column='taskId', blank=True, null=True)  # Field name made lowercase.
+#     userid = models.ForeignKey('User', models.DO_NOTHING, db_column='userId', blank=True, null=True)  # Field name made lowercase.
+#
+#     class Meta:
+#         managed = False
+#         db_table = 'shareTask'
+
+
+class Storage(models.Model): #硬盘
+    devicename = models.CharField(max_length=50, blank=True, null=True)  #硬盘设备名称
+    manufactor = models.CharField(max_length=50, blank=True, null=True) #制造商
+    capacity = models.CharField(max_length=50, blank=True, null=True) #容量
+    sectorsize = models.CharField(max_length=50, blank=True, null=True) #扇区大小
+    config = models.ForeignKey(Config)  # 配置
+
+    class Meta:
+        managed = False
+        db_table = 'storage'
+
+
+class System(models.Model): #系统
+    name = models.CharField(max_length=50, null=False) #系统名称
+    manufacturer = models.CharField(max_length=200, blank=True, null=True)#制造商
+    version = models.CharField(max_length=50, blank=True, null=True) #系统版本
+
+    class Meta:
+        managed = False
+        db_table = 'system'
+
+
+class Task(models.Model): #测试任务
+    owner = models.ForeignKey(User) #拥有者
+    config = models.ForeignKey(Config)  # 测试配置
+    time = models.DateTimeField(auto_created=True) #上传日期
+    remark = models.CharField(max_length=50, blank=True, null=True) #备注
+    delete = models.BooleanField(default=False)  # 是否删除
+    name = models.CharField(max_length=50, null=False) #测试任务名称
+    shareusers = models.ManyToManyField(User,db_column='shareTask') #共享表和用户的多对多关系
+    class Meta:
+        managed = False
+        db_table = 'task'
+
+
+class TestCase(models.Model): #用例表
+    tool = models.ForeignKey(TestTool)  # 测试工具
+    scenario = models.ForeignKey(Scenario)  # 场景
+    name = models.CharField(max_length=50,  null=False) #用例名称
+    desc = models.CharField(max_length=200, blank=True, null=True) #用例描述
+
+    class Meta:
+        managed = False
+        db_table = 'testCase'
+
+
+class TestResult(models.Model):
+    case = models.IntegerField(db_column='caseId', blank=True, null=True)  # 测试用例
+    result = models.CharField(max_length=255, blank=True, null=True) # 测试得分
+    taskid = models.IntegerField(db_column='taskId', blank=True, null=True)  # Field name made lowercase.
+    unit = models.CharField(max_length=50, blank=True, null=True)
+    firstresult = models.CharField(db_column='firstResult', max_length=50, blank=True, null=True)  # Field name made lowercase.
+    caseconfig = models.CharField(db_column='caseConfig', max_length=255, blank=True, null=True)  # Field name made lowercase.
+    dimensionresult = models.CharField(db_column='dimensionResult', max_length=255, blank=True, null=True)  # Field name made lowercase.
+
+    class Meta:
+        managed = False
+        db_table = 'testResult'
+
+
+class TestTool(models.Model):
+    name = models.CharField(max_length=50,null=False) #工具名称
+    desc = models.CharField(max_length=200, blank=True, null=True) #工具描述
+
+    class Meta:
+        managed = False
+        db_table = 'testTool'
