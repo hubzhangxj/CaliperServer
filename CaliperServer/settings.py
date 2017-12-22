3b514af9dd9b5f3fb26847cd262e63193d7b4bbb
qiaohua.qiu <1qazxsw2>
2017-12-22 16:03:40 +0800

    delete rest_framework

--- CaliperServer/settings.py
+++ CaliperServer/settings.py
@@ -58,7 +58,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
     'shared',
     'account',
     'task',
