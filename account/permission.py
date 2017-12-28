bfc53c1bca39f7147958e294c1274b14ab12466b
qiaohua.qiu <1qazxsw2>
2017-12-28 14:09:05 +0800

    加入登录校验

---
+++ account/permission.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+from django.shortcuts import render, HttpResponseRedirect
+
+def login_required(fun):
+    """
+    检查登录装饰器，如果没有登录就渲染unlogin界面
+    """
+    def wapper(request, *args, **kwargs):
+        if request.user is None or request.user.is_anonymous():
+            return HttpResponseRedirect('/')
+            # return HttpResponseRedirect('/static/unlogin.html')
+
+        return fun(request, *args, **kwargs)
+    return wapper
