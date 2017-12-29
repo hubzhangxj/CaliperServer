64cde96c3bcc77babcc2a1a9ac378398e31e557d
qiaohua.qiu <1qazxsw2>
2017-12-29 17:28:14 +0800

    依赖和114 配置

--- push.sh
+++ push.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-sshpass -p zuzhewaner123! rsync -avz --exclude-from='exclude.list' -e ssh -r ../CaliperServer root@106.12.27.16:/home/ts
-sshpass -p zuzhewaner123! ssh root@106.12.27.16 'chown -R www-data /home/ts/CaliperServer'
-#sshpass -p zuzhewaner123! ssh root@106.12.27.16 'python /home/ts/Estuary/manage.py compilemessages'
-sshpass -p zuzhewaner123! ssh root@106.12.27.16 'service apache2 restart'
+#sshpass -p zuzhewaner123! rsync -avz --exclude-from='exclude.list' -e ssh -r ../CaliperServer root@106.12.27.16:/home/ts
+#sshpass -p zuzhewaner123! ssh root@106.12.27.16 'chown -R www-data /home/ts/CaliperServer'
+#sshpass -p zuzhewaner123! ssh root@106.12.27.16 'service apache2 restart'
 
 #rsync -avz --exclude-from='exclude.list' -e 'ssh -p 222' -r ../Estuary litao@htsat.vicp.cc:/home/litao
 #rsync -avz --exclude-from='exclude.list' -e ssh -r ../Estuary root@192.168.100.204:/root
 
 #echo '123' | sudo -S apt-get update
+
+
+
+sshpass -p tzy@123 rsync -avz --exclude-from='exclude.list'  -e ssh -r ../CaliperServer root@114.119.11.94:/home/ts
+sshpass -p tzy@123 ssh root@114.119.11.94  'chown -R www-data /home/ts/CaliperServer'
+sshpass -p tzy@123 ssh root@114.119.11.94 'service apache2 restart'
