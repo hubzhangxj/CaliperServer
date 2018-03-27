43d7b78bbdd525d9f9272170ae69757fed0d3ac6
qiaohua.qiu <1qazxsw2>
2018-03-27 14:38:55 +0800

    登录异常处理

--- account/views.py
+++ account/views.py
@@ -196,16 +196,20 @@ def auth_callback(request):
 
     error, user, user_info = SSOAuthBackend.authenticate(auth_token)
     if not error:
-        if not user:  # 这种情况表明用户在其他site注册，并且首次登陆本site
-            role = Contants.ROLE_USER
-            if user_info is not None and user_info['admin'] is not None and request.get_host().lower() in user_info['admin'].lower():
-                role = Contants.ROLE_ADMIN
-            user = UserProfile(username=user_info['username'], email=user_info['email'], role=role)
-            user.save()
-        auth.login(request, user)  # create session, write cookies
-        logined_users[auth_token] = user  # 存入全局变量中
-        request.session["auth_token"] = auth_token  # 存入session
-        return HttpResponseRedirect(redirect)
+        try:
+            if not user:  # 这种情况表明用户在其他site注册，并且首次登陆本site
+                role = Contants.ROLE_USER
+                if user_info is not None and user_info['admin'] is not None and request.get_host().lower() in user_info['admin'].lower():
+                    role = Contants.ROLE_ADMIN
+                user = UserProfile(username=user_info['username'], email=user_info['email'], role=role)
+                user.save()
+            auth.login(request, user)  # create session, write cookies
+            logined_users[auth_token] = user  # 存入全局变量中
+            request.session["auth_token"] = auth_token  # 存入session
+            return HttpResponseRedirect(redirect)
+        except Exception as e:
+            logger.info(str(e))
+            return HttpResponse('sso authenticate failed')
     else:
         raise PermissionDenied
 
